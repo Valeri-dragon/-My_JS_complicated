46608bf35a7d95dff0fb06c4549c262537e63bd6
Valeriya Terebilova <yakontekst01@yandex.ru>
2023-02-03 21:50:25 +0300

    changed the code according to the comments of the curator

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const num = 266219;
-let numStr = new String(num).split("");
+const numStr = new String(num).split("");
 let score = 1;
 for (let i = 0; i < numStr.length; i++) {
   console.log((score = score * Number(numStr[i])));
 }
-let degree = score ** 3;
+const degree = score ** 3;
 console.log(degree);
-let strDegree = degree.toString();
-console.log(Number(strDegree.slice(0,2)))
+console.log(Number(degree.toString().slice(0,2)))
